0449f396460620f25a06174d43ee5922589ae53f
Alexander Robinson <robinson@ucm.es>
2023-04-27 07:04:24 +0200

    Added mask loading.

--- src/climatrix.f90
+++ src/climatrix.f90
@@ -20,8 +20,8 @@ module climatrix
 
     type climatrix_field
         character(len=56) :: name
-        real(wp), allocatable :: mask(:,:,:,:)      ! [ng,nc,nx,ny]
         real(wp), allocatable :: z_srf(:,:,:,:)     ! [ng,nc,nx,ny]
+        real(wp), allocatable :: mask(:,:,:,:)      ! [ng,nc,nx,ny]
         real(wp), allocatable :: var(:,:,:,:)       ! [ng,nc,nx,ny]
     end type
 
@@ -33,8 +33,6 @@ module climatrix
         real(wp), allocatable :: x_geom(:)          ! [ng]
         real(wp), allocatable :: x_clim(:)          ! [nc]
 
-        real(wp), allocatable :: wt(:,:)            ! [ng,nc] 
-
         ! All fields are for a given month, or can represent annual fields too
         !type(climatrix_field) :: t2m
         !type(climatrix_field) :: pr
@@ -45,6 +43,81 @@ module climatrix
     public 
 
 contains
+
+    subroutine climatrix_interp(var,z_srf,x_geom,x_clim,name,cax)
+
+        implicit none
+
+        real(wp), intent(OUT) :: var(:,:)           ! [nx,ny] Calculated field
+        real(wp), intent(IN)  :: z_srf(:,:)         ! [nx,ny] Current surface topography
+        real(wp), intent(IN)  :: x_geom             ! Current x_geom value
+        real(wp), intent(IN)  :: x_clim             ! Current x_clim value
+        character(len=*), intent(IN) :: name
+        type(climatrix_class), intent(IN) :: cax 
+
+        ! Local variables
+        integer  :: i, j
+        integer  :: i0, i1, j0, j1 
+        real(wp) :: wt_geom, wt_clim
+        type(climatrix_field) :: fld
+        
+        ! Determine which field variable is being interpolated,
+        ! extract field information from climatrix object
+        select case(trim(name))
+
+            case("smb")
+                fld = cax%smb 
+
+            case DEFAULT
+                write(error_unit,*) "climatrix_interp:: field name not recognized."
+                write(error_unit,*) "name = ", trim(name)
+                stop
+
+        end select
+
+        ! Determine relevant axis indices bounding (x_geom,x_clim)
+
+        if (x_geom .lt. minval(cax%x_geom)) then 
+            i0 = 1
+            i1 = 1 
+        else if (x_geom .gt. maxval(cax%x_geom)) then 
+            i0 = cax%p%ng 
+            i1 = cax%p%ng 
+        else 
+            do i = 1, cax%p%ng 
+                if (cax%x_geom(i) .ge. x_geom) then 
+                    i0 = i-1
+                    i1 = i 
+                    exit 
+                end if
+            end do 
+        end if
+
+        if (x_clim .lt. minval(cax%x_clim)) then 
+            j0 = 1
+            j1 = 1 
+        else if (x_clim .gt. maxval(cax%x_clim)) then 
+            j0 = cax%p%ng 
+            j1 = cax%p%ng 
+        else 
+            do j = 1, cax%p%ng 
+                if (cax%x_clim(j) .ge. x_clim) then 
+                    j0 = j-1
+                    j1 = j 
+                    exit 
+                end if
+            end do 
+        end if
+
+        write(output_unit,*) "x_geom: ", x_geom, cax%x_geom(i0), cax%x_geom(i1)
+        write(output_unit,*) "x_clim: ", x_clim, cax%x_clim(i0), cax%x_clim(i1)
+
+
+        return
+
+    end subroutine climatrix_interp
+
+
 
     subroutine climatrix_init(cax,filename,group)
 
@@ -60,15 +133,12 @@ contains
 
         if (allocated(cax%x_geom))  deallocate(cax%x_geom)
         if (allocated(cax%x_clim))  deallocate(cax%x_clim)
-        if (allocated(cax%wt))      deallocate(cax%wt)
-        
+
         allocate(cax%x_geom(cax%p%ng))
         allocate(cax%x_clim(cax%p%nc))
-        allocate(cax%wt(cax%p%ng,cax%p%nc))
-        
+
         cax%x_geom = MV
         cax%x_clim = MV
-        cax%wt     = 0.0 
 
         ! === Initialize climatrix field variables === 
 
@@ -99,9 +169,15 @@ contains
         character(len=56)  :: fldrs(100)
         character(len=256) :: file 
         character(len=56)  :: zs_name 
+        character(len=56)  :: mask_name 
         character(len=56)  :: var_name 
         character(len=512) :: file_path 
+        real(wp)           :: mask_val 
 
+        real(wp), allocatable :: mask_in(:,:) 
+
+        allocate(mask_in(cax%p%nx,cax%p%ny))
+        
         xc = MV
 
         call nml_read(filename,group,"xg",xg)
@@ -110,7 +186,9 @@ contains
         call nml_read(filename,group,"fldrs",fldrs)
         call nml_read(filename,group,"file",file)
         call nml_read(filename,group,"zs_name",zs_name)
+        call nml_read(filename,group,"mask_name",mask_name)
         call nml_read(filename,group,"var_name",var_name)
+        call nml_read(filename,group,"mask_val",mask_val)
         
         ! Determine how many x_clim values there are
         nc = count(xc .ne. MV)
@@ -127,7 +205,9 @@ contains
         call nml_print("fldrs",fldrs(1:nc))
         call nml_print("file",file)
         call nml_print("zs_name",zs_name)
+        call nml_print("mask_name",mask_name)
         call nml_print("var_name",var_name)
+        call nml_print("mask_val",mask_val)
         
         write(output_unit,*)
         
@@ -155,11 +235,17 @@ contains
 
             file_path = trim(path)//"/"//trim(fldrs(j))//"/"//trim(file)
 
-            call nc_read(trim(file_path),zs_name, cax%smb%z_srf(i,j,:,:),start=[1,1,1],count=[cax%p%nx,cax%p%ny,1])
-            call nc_read(trim(file_path),var_name,cax%smb%var(i,j,:,:),start=[1,1,1],count=[cax%p%nx,cax%p%ny,1])
+            call nc_read(trim(file_path),zs_name,  cax%smb%z_srf(i,j,:,:),start=[1,1,1],count=[cax%p%nx,cax%p%ny,1])
+            call nc_read(trim(file_path),var_name, cax%smb%var(i,j,:,:),  start=[1,1,1],count=[cax%p%nx,cax%p%ny,1])
+            call nc_read(trim(file_path),mask_name,mask_in,start=[1,1,1],count=[cax%p%nx,cax%p%ny,1])
             
+            ! Get ice mask
+            cax%smb%mask(i,j,:,:) = 0.0
+            where( abs(cax%smb%mask(i,j,:,:)-mask_in) .lt. TOL) cax%smb%mask(i,j,:,:) = 1.0
+
             write(output_unit,*) "Loaded slice, i, j: ", i, j 
             write(output_unit,*) "    z_srf: ", minval(cax%smb%z_srf(i,j,:,:)), maxval(cax%smb%z_srf(i,j,:,:))
+            write(output_unit,*) "    mask: ", minval(cax%smb%mask(i,j,:,:)), maxval(cax%smb%mask(i,j,:,:))
             write(output_unit,*) "      var: ", minval(cax%smb%var(i,j,:,:)),   maxval(cax%smb%var(i,j,:,:))
             
         end do 
@@ -214,12 +300,12 @@ contains
         call climatrix_field_dealloc(fld)
 
         ! Allocate field variables
-        allocate(fld%mask(ng,nc,nx,ny))
         allocate(fld%z_srf(ng,nc,nx,ny))
+        allocate(fld%mask(ng,nc,nx,ny))
         allocate(fld%var(ng,nc,nx,ny))
         
-        fld%mask  = MV 
         fld%z_srf = MV 
+        fld%mask  = MV 
         fld%var   = MV 
 
         return
@@ -232,8 +318,8 @@ contains
 
         type(climatrix_field), intent(INOUT) :: fld
 
-        if (allocated(fld%mask))  deallocate(fld%mask)
         if (allocated(fld%z_srf)) deallocate(fld%z_srf)
+        if (allocated(fld%mask))  deallocate(fld%mask)
         if (allocated(fld%var))   deallocate(fld%var)
         
         return
--- tests/test_climatrix.f90
+++ tests/test_climatrix.f90
@@ -8,6 +8,12 @@ program test_climatrix
     character(len=256) :: par_path
     character(len=256) :: par_slice_path
 
+    real(wp) :: x_geom
+    real(wp) :: x_clim 
+    real(wp), allocatable :: z_srf(:,:) 
+    real(wp), allocatable :: mask(:,:) 
+    real(wp), allocatable :: smb(:,:) 
+
     ! ===========================================
     write(*,*) " "
     write(*,*) " Program: test_climatrix"
@@ -23,6 +29,8 @@ program test_climatrix
     cax%x_geom  = [0.0, 20.0, 50.0, 80.0, 100.0]
     cax%x_clim  = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0]
 
+    ! Load matrix data
+
     call climatrix_load_field_slice(cax,par_slice_path,"sims100",x_geom=100.0_wp)
     call climatrix_load_field_slice(cax,par_slice_path,"sims080",x_geom=80.0_wp)
     call climatrix_load_field_slice(cax,par_slice_path,"sims050",x_geom=50.0_wp)
@@ -32,8 +40,23 @@ program test_climatrix
     write(*,*)
     write(*,*) "All input data loaded."
     write(*,*) "    z_srf: ", minval(cax%smb%z_srf), maxval(cax%smb%z_srf)
+    write(*,*) "     mask: ", minval(cax%smb%mask),  maxval(cax%smb%mask)
     write(*,*) "      smb: ", minval(cax%smb%var),   maxval(cax%smb%var)
     
+    ! Perform interpolation to a given location (x_geom,x_clim)
+
+    allocate(z_srf(cax%p%nx,cax%p%ny))
+    allocate(mask(cax%p%nx,cax%p%ny))
+    allocate(smb(cax%p%nx,cax%p%ny))
+
+    x_geom = 70.0 
+    x_clim =  2.0 
+
+    ! Load variable info
+
+    !call climatrix_interp(var,z_srf,H_ice,x_geom,x_clim,name,cax)
+
+
     write(*,*)
     write(*,*) " test_climatrix complete."
     write(*,*)
